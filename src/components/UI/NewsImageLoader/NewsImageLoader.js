48d2a74502447a728d7af0475a1e9bc2d212491b
Свежинцева Надежда Анатольевна <n.svezhintseva.46@fss.local>
2019-04-19 11:30:02 +0300

    Fix bugs in an image component in News

--- src/components/UI/NewsImageLoader/NewsImageLoader.js
+++ src/components/UI/NewsImageLoader/NewsImageLoader.js
@@ -3,6 +3,8 @@ import Button from '../Button/Button';
 
 import classes from './NewsImageLoader.css'
 
+const IMAGE_WIDTH = 350
+
 class NewsImageLoader extends Component {
 
   constructor(props) {
@@ -14,6 +16,7 @@ class NewsImageLoader extends Component {
 
     this.fileInput = React.createRef()
     this.image = React.createRef()
+    this.imageWidth = 350;
   }
 
   deleteHandler = ()=> {
@@ -24,15 +27,27 @@ class NewsImageLoader extends Component {
   ImageChangedHandler = (event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
-      const reader = new FileReader();
+      const reader = new FileReader()
 
       reader.onload = (event) => {
         this.image.current.src = event.target.result
+        var img = new Image();
+
+        img.onload = () => {
+            if (img.width < IMAGE_WIDTH) {
+              this.imageWidth = img.width
+              this.image.current.width = this.imageWidth
+            } else {
+              this.image.current.width = IMAGE_WIDTH
+            }
+        };
+
+        img.src = reader.result;
       };
 
       this.setState({showImage: true},
           () => {
-            reader.readAsDataURL(selectedFile);
+            reader.readAsDataURL(selectedFile)
             this.props.imageChangeHandler(selectedFile)
           }
       )
@@ -43,6 +58,7 @@ class NewsImageLoader extends Component {
   render() {
       const dt = new Date();
       const param = dt.toISOString();
+      const src = (this.image.current) ? this.image.current.src : this.props.link+`?${param}`
       return (
         <div className={classes.newsImageLoaderWrapper}>
             <input type="file"
@@ -66,7 +82,7 @@ class NewsImageLoader extends Component {
            {(this.state.showImage) ?
              <div  className={classes.iconImage}>
                     <div className={classes.delete} onClick = { this.deleteHandler } ></div>
-                    <img src={this.props.link+`?${param}`} alt="Фото" ref={this.image} width="350"/>
+                    <img src={src} alt="Фото" ref={this.image}/>
              </div>
              : null
            }
